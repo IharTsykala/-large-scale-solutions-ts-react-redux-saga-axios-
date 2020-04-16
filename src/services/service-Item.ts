1430a8c5918d3434ef25c146b4f82e133f6cf058
Ihar Tsykala <Sudja09@bk.ru>
2020-04-16 14:02:46 +0300

    feat: add prev deploy

--- src/services/service-Item.ts
+++ src/services/service-Item.ts
@@ -1,27 +1,16 @@
 const axios = require("axios")
 
 export default class ServiceItem {
-  // static interceptor = axios.interceptors.request.use(
-  //   function (config: any) {
-  //     const token = localStorage.getItem("token")
-  //     if (token) {
-  //       config.headers.Authorization = `Bearer ${token}`
-  //     }
-  //     return config
-  //   },
-  //   function (error: string) {
-  //     return Promise.reject(error)
-  //   }
-  // )
-
   static getListItemsFromDB = async () => {
     try {
-      const response = await axios.get(`https://mrsoft.by/tz20/list.json`)
-      return response
+      const response = await axios(`https://mrsoft.by/tz20/list.json`)
+      console.log(response)
+      return response.data
     } catch (e) {
       console.log(e)
     }
   }
+
   static getDescriptionCurrentItemFromDB = async (
     basePath: string,
     userLink: string
